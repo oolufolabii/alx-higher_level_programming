dabc7624e706264bd8e13dddaf9f8c63d09f630f
oolufolabii <otitoola.olufolabi@gmail.com>
2022-06-28 13:56:45 +0100

    Python Loop

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def remove_char_at(str, n):
+    new_string = ''
+    index = 0
+    for char in str:
+        if index != n:
+            new_string += str[index]
+        i += 1
+    return new_string
