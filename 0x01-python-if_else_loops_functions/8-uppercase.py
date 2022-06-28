6be29eb2d2d2ccfe0ec98b323cae2975710055aa
oolufolabii <otitoola.olufolabi@gmail.com>
2022-06-28 12:30:19 +0100

    Python Loop

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
             char = chr(ord(c) - 32)
         else:
             char = c
-        print("{:s}".format(char), end="")
+        print(f"{char:s}", end="")
     print('')
