dfcae79fea9208d3b29021fcd68bf7dd227b4912
oolufolabii <otitoola.olufolabi@gmail.com>
2022-07-01 11:03:15 +0100

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    if len(my_list) == 0:
+        return None
+    [new_list.append(my_list[num] % 2 == 0) for num in range(len(my_list)-1)]
+    return new_list
