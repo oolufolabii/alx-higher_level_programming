b29a558a13f9ec8dfcd07b7575f8edda31976571
oolufolabii <otitoola.olufolabi@gmail.com>
2022-07-06 10:37:09 +0100

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,9 +7,3 @@ def search_replace(my_list, search, replace):
         else:
             replacement_list.append(item)
     return replacement_list
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
