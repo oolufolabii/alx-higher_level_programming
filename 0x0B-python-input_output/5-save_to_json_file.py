bae54ec2cfa31ddbb17cc59f1d5c26207b80a5cb
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-02 10:58:45 +0100

    Python - Input/Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save_to_json_file(my_obj, filename)
+
+    Args:
+        my_obj (JSON): JSON object to write
+        filename (file): file to write into
+    """
+    
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
