862b9322b7763120c7635251cf64d55ec1c7f3ee
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-02 14:05:21 +0100

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""a module that adds all arguments to a Python list,
-    and then save them to a file.
-    """
+"""0x0B. Python - Input/Output, task 9. Load, add, save  """
 
-import sys
-import json
-import os.path
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = 'add_item.json'
-
-argv_list = sys.argv[1:]
+argv_edit = argv[1:]
 
 try:
-    content = load_from_json_file(file)
+    content_list = load_from_json_file("add_item.json")
 except:
-    content = []
+    content_list = []
 finally:
-    for arg in argv_list:
-        content.append(arg)
-    save_to_json_file(content, "add_item.json")
+    for arg in argv_edit:
+        content_list.append(arg)
+    save_to_json_file(content_list, "add_item.json")
