8d26ebf8c66444ca776c1bd17b79c9c2c5c59385
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-02 12:18:04 +0100

    Python - Input/Output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""a python function that inserts a line of text to a file,
+after each line containing a specific string
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append_after(filename, search_string, new_string)
+
+    Args:
+        filename: name of the file
+        search_string: string to append after
+        new_string: new_string to append
+    """
+
+    with open(filename, "r") as file:
+        text = file.readlines()
+
+    with open(filename, "w") as file_w:
+        text = ""
+        for line in text:
+            text += line
+            if search_string in line:
+                text += new_string
+        file_w.write(text)
