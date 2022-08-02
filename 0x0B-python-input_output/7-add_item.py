a560787eb6a53239944208723bdd97964ce22865
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-02 13:56:01 +0100

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,13 +12,13 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 file = 'add_item.json'
 
-arg_list = []
+argv_list = sys.argv[1:]
 
-if os.path.exists(file) and os.path.getsize(file) > 0:
-    arg_list = load_from_json_file(file)
-
-if len(sys.argv) > 1:
-    for item in sys.argv[1:]:
-        arg_list.append(item)
-
-save_to_json_file(arg_list, file)
+try:
+    content = load_from_json_file(file)
+except:
+    content = []
+finally:
+    for arg in argv_list:
+        content.append(arg)
+    save_to_json_file(content, "add_item.json")
