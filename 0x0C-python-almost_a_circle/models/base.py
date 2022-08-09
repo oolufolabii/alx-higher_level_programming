45bb8fe6933d8c50cfef671ba30a047e16f9d9aa
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-09 10:36:44 +0100

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,13 +19,33 @@ class Base:
         Raises:
             TypeError: if id is not an integer, raises error
         """
-
-        if type(id) is not int:
-            raise TypeError("id must be an integer")
-
         if id is not None:
+            if type(id) is not int:
+                raise TypeError("id must be an integer")
             self.id = id
+        
+        # if type(id) is not int:
+        #     raise TypeError("id must be an integer")
+
+        # if id is not None:
+        #     self.id = id
 
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+b1 = Base()
+print(b1.id)
+
+b2 = Base()
+print(b2.id)
+
+b3 = Base()
+print(b3.id)
+
+b4 = Base(12)
+print(b4.id)
+
+b5 = Base()
+print(b5.id)
