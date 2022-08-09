6cbccfc5390e481af1ead42f24fe592dea27cfbf
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-09 10:39:47 +0100

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,19 +30,3 @@ class Base:
         # else:
         #     Base.__nb_objects += 1
         #     self.id = Base.__nb_objects
-
-
-b1 = Base()
-print(b1.id)
-
-b2 = Base()
-print(b2.id)
-
-b3 = Base()
-print(b3.id)
-
-b4 = Base(12)
-print(b4.id)
-
-b5 = Base()
-print(b5.id)
