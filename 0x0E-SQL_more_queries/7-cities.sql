280cdf78ee1bbd8483cd30e5726317de5720abeb
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-07 20:05:39 +0100

    Sql Queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE hbtn_0d_usa
-USE hbtn_0d_usa
+-- a script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
