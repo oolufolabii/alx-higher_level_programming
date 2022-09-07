8b18c466f0a8698a49fd156423bb3bc47712969e
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-07 21:41:33 +0100

    Sql Queries

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- Display records from two tables with a matching field
-SELECT a.name
-    FROM tv_genres a
+SELECT s.name
+    FROM tv_genres s
     INNER JOIN tv_show_genres b
-        ON a.id = b.genre_id
+        ON s.id = b.genre_id
     INNER JOIN tv_shows c
         ON c.id = b.show_id
     WHERE c.title = 'Dexter'
-    ORDER BY a.name ASC;
+    ORDER BY s.name ASC;
