7fe0a1044f0717370b68df79e22aabeaa5ef333d
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-03 10:54:22 +0100

    Javascript warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,4 +4,6 @@ const cLang = 'C is fun';
 const python = 'Python is cool';
 const javaScript = 'JavaScript is amazing';
 
-console.log(cLang,'\n', python,'\n', javaScript);
+console.log(cLang, '\n',
+	python, '\n',
+	javaScript);
