f4e62b46250b11197886c76724c305e0efb73132
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-03 16:44:17 +0100

    Javascript warm up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 function add(a, b) {
-	const sum = a + b;
-	console.log(sum);
+  const sum = a + b;
+  console.log(sum);
 }
 const argsChecker1 = Number.parseInt(process.argv[2]);
 const argsChecker2 = Number.parseInt(process.argv[3]);
