26f337b0dfab3b24813e31fef9c6cd7fe779204e
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-04 19:06:45 +0100

    Rectangle class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,7 +11,7 @@ class Square extends SquareMaster {
     if (c === undefined) {
       console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
     } else {
-      console.log(('c'.repeat(this.width) + '\n').repeat(this.height - 1) + 'c'.repeat(this.width));
+      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
     }
   }
 }
