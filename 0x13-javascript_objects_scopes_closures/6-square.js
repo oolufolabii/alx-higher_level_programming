90f483eba88b09920637f31cde558d6525b2e721
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-04 18:39:32 +0100

    Rectangle class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const SquareMaster = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareMaster {
   constructor (size) {
     super(size, size);
   }
