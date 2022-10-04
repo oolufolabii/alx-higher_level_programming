47ab9d5d08c60d9fd8aaa57f64b00abd4ba0bedd
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-04 15:02:15 +0100

    Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if ((w = parseInt(w)) > 0 && (h = parseInt(h) > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+  }
+
+  rotate() {
+    const w = this.width;
+    const h = this.height;
+
+    this.width = h;
+    this.height = w;
+
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+}
+
+module.exports = Rectangle;
