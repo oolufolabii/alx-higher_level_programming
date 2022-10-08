7bf1f77010c95917a4c89888b4f158a2d1e8dcbd
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-08 13:43:40 +0100

    ORM Scripts

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,8 +4,6 @@ a python file that contains the class definition
 of a State and an instance Base = declarative_base()
 '''
 
-
-from enum import unique
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
