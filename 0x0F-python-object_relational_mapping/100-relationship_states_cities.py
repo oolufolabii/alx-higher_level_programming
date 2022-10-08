e35788e0f9eac6176732d13d3034f2778b4945e1
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-08 21:02:01 +0100

    ORM Scripts

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,27 +5,28 @@ Script that creates the State “California” with the City
 '''
 
 if __name__ == '__main__':
-    
+
     import sys
     from relationship_state import Base, State
     from relationship_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
     from sqlalchemy.schema import Table
-    
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
-    
+
     session = Session(engine)
-    
+
     new_city_name = City(name='San Francisco')
-    
+
     new_state_name = State(name='California')
     new_state_name.cities.append(new_city_name)
-    
+
     session.add_all([new, new_city_name])
-    
+
     session.commit()
-    
+
     session.close()
