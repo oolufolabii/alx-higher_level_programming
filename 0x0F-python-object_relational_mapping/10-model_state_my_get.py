654ddc690a40f532de239eac37631389b6ec58f8
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-08 12:21:50 +0100

    ORM Scripts

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         .first()
 
     if state:
-        print('{}: {}'.format(state.id))
+        print('{}'.format(state.id))
     else:
         print('Not found')
 
