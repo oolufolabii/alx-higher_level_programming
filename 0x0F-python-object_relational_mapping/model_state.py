f8e428c16d6725e9f22ddc22dcf34c1b1fea38c6
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-08 10:45:31 +0100

    ORM Scripts

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+'''
+a python file that contains the class definition
+of a State and an instance Base = declarative_base()
+'''
+
+
+from enum import unique
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''A state class with name and id'''
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
