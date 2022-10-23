c9fb262276cc71589d272d002aa5892c8a3bebba
Olufolabi <101478509+oolufolabii@users.noreply.github.com>
2022-10-23 07:33:58 +0100

    Add files via upload

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html_request = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html_request)))
+        print('\t- content: {}'.format(html_request))
+        print('\t- utf8 content: {}'.format(html_request.decode("utf-8", "replace")))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+# Tasks on 0x11. Python - Network #1
+
+---
