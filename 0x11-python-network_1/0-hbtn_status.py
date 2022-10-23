5ea0f224163fba4296c6aef4c744b5c35c465d96
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-23 08:53:10 +0100

    Python Network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
 
 if __name__ == "__main__":
 
-	import urllib.request
+    import urllib.request
 
-	with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-		html_request = response.read()
-		print('Body response:')
-		print('\t- type: {}'.format(type(html_request)))
-		print('\t- content: {}'.format(html_request))
-		print('\t- utf8 content: {}'.format(html_request.decode("utf-8", "replace")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html_request = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html_request)))
+        print('\t- content: {}'.format(html_request))
+        print('\t- utf8 content: {}'.format(html_request.decode("utf-8", "replace")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request
 to the URL and displays the value of the X-Request-Id
