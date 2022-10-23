f8a74930919592fc1bc3f3a51fc1124b5d5badd3
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-23 08:46:45 +0100

    Python Network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == '__main__':
-    html_response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(html_response.text)))
-    print('\t- content: {}'.format(html_response.text))
+	html_response = requests.get('https://intranet.hbtn.io/status')
+	print('Body response:')
+	print('\t- type: {}'.format(type(html_response.text)))
+	print('\t- content: {}'.format(html_response.text))
