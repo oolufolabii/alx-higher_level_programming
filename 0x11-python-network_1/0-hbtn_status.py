268b3f22389194fa7d3e81dcb7f7cdd273392162
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-24 18:26:48 +0100

    Python Networks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Sends a request to a URL and prints its response or error code."""
 
 import sys
