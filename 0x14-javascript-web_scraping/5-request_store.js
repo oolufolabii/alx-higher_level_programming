70c03c64a5e685c895487dda2e455d7aaf1be5d0
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-16 11:20:51 +0100

    Web scrapping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,12 +3,12 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], (err, response, body) => {
-	if (err) {
-		console.error(err);
-	}
-	try {
-		fs.writeFile(process.argv[3], body, 'utf8', (err, result) => { if (err) console.log(err); });
-	} catch (err) {
-		console.log(err);
-	}
+  if (err) {
+    console.error(err);
+  }
+  try {
+    fs.writeFile(process.argv[3], body, 'utf8', (err, result) => { if (err) console.log(err); });
+  } catch (err) {
+    console.log(err);
+  }
 });
